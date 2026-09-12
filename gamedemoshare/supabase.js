53f6ae75b8f5aa60d5db28f3eb45d4a9b52e9d2c
huanyu208 <zhoulou_1219qqcom@qq.com>
2026-09-12 23:12:42 +0800

    Delete gamedemoshare directory

--- gamedemoshare/supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASEURL = "https://kczxtbmgigbfofdzkynm.supabase.co";
-const SUPABASEANONKEY = "sb_publishable_jiyD19NDY1DEc7jVFOQzaw_I-85Ncte";
-const { createClient } = window.supabase;
-const supabaseClient = createClient(SUPABASEURL, SUPABASEANONKEY);
