10e8a4403c6ede9830945a370dd2eda28340a411
huanyu208 <zhoulou_1219qqcom@qq.com>
2026-09-12 23:02:21 +0800

    Add files via upload

---
+++ gamedemoshare/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASEURL = "https://kczxtbmgigbfofdzkynm.supabase.co";
+const SUPABASEANONKEY = "sb_publishable_jiyD19NDY1DEc7jVFOQzaw_I-85Ncte";
+const { createClient } = window.supabase;
+const supabaseClient = createClient(SUPABASEURL, SUPABASEANONKEY);
